d206926a788fb002b986875e9fcad517ec8f4a80
Raphael <raphael.mp3@hotmail.com>
2021-07-18 20:32:54 -0300

    segundo commit frontend

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,38 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import api from '../services/api';
 import logo from '../assets/logo.svg';
 import './login.css';
 
-export default function Login(){
+
+
+export default function Login({ history }){
+
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        const response = await api.post('/devs',{
+            username
+        });
+
+        const { _id } = response.data;
+
+
+
+        history.push(`/dev/${_id}`);
+
+    }
+
     return(
         <div className="login-container">
-            <form>
-                <img src={logo} alt="Tindev"/>
-                <input placeholder="Digite seu usuario do Github"/>
+            <form onSubmit={handleSubmit}>
+                <img src={logo} className="logo" alt="Tindev"/>
+                <input 
+                placeholder="Digite seu usuario do Github"
+                value={username}
+                onChange={e => setUsername( e.target.value)}
+                />
                 <button type="submit">Enviar</button>
                 
             </form>
